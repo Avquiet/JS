2a1d7f757f938e84198fe2e7890e8f58161825cf
Avquiet <78691610+Avquiet@users.noreply.github.com>
2021-05-07 15:09:42 +0300

    added 2

--- main.js
+++ main.js
@@ -4,3 +4,11 @@ let Tf = (9 / 5) * Tc + 32;
     alert('Температура по Фаренгейту: ' + Tf);
 
 //2
+let a = 7;
+let b = 10;
+
+console.log (a, b);
+    b = [a, a = b][0];
+console.log (a, b);
+
+//3
