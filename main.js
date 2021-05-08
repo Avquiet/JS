7ba4f04fc7889f69afd95d428d299cc45c4f5c0c
Avquiet <78691610+Avquiet@users.noreply.github.com>
2021-05-08 16:31:16 +0300

    Homework js 08.05.21

--- main.js
+++ main.js
@@ -1,14 +1,27 @@
 //1
-let Tc = 14;
+alert('Задание: №1 ');
+let Tc = prompt('Введите температуру по цельсию: ');
 let Tf = (9 / 5) * Tc + 32;
-    alert('Температура по Фаренгейту: ' + Tf);
+alert('Температура по Фаренгейту: ' + Tf);
 
 //2
-let a = 7;
-let b = 10;
-
-console.log (a, b);
-    b = [a, a = b][0];
-console.log (a, b);
+alert('Задание: №2')
+let a = 1;
+let b = 2;
+a = a + b; 
+b = a - b; 
+a = a - b;
+alert(a);
+alert(b);
 
 //3
+alert('Задание: №3 ');
+let admin;
+let name = 'Василий';
+admin = name;
+alert('Ваше имя: ' + admin);
+
+//4
+alert('Задание: №4');
+let numText = 1000 + '108';
+alert(numText); //ответ 1000108
