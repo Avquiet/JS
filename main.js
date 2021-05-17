6d6dcc00dc05e093e53e8ac5907b0cda14634c9a
Avquiet <78691610+Avquiet@users.noreply.github.com>
2021-05-17 21:49:05 +0300

    added hw 17.5.21
    js

--- main.js
+++ main.js
@@ -1,142 +1,52 @@
-// 1 
-var a = 1, b = 1, c, d;
-c = ++a; alert(c);           // 2  a++ прибавляет к a=1 еще +1.
-d = b++; alert(d);           // 1  потому что b++ возвращает исходное значение.
-c = (2+ ++a); alert(c);      // 5  2+ и еще +2 к a=1, потому что плюсуется в с первым примером.
-d = (2+ b++); alert(d);      // 4  тоже самое..
-alert(a);                    // 3  ++ увеличивает на +2 к a=1.
-alert(b);                    // 3  тоже самое.. как на a=1.
-
-
-// 2
-var a = 2;
-var x = 1 + (a *= 2);
-//ОТВЕТ: 5;
-
-// 3
-var a = 16;
-var b = 12;
- 
-if (a > 0 && b > 0) {
- 	a - b; //4
-
-} else if (a < 0 && b < 0) {
-	a * b; //184
-
-} else if(a > 0 && b < 0 || a < 0 && b > 0) {
-	a + b; //28
+// 1
+var n = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
+var i = 0;
+while ( i < n.length) {
+	console.log(n[i++]);
 }
 
-//4
-var a = +prompt('Введите число:  (от 0 до 15) ');
-switch(a) {
-	case 0:
-		alert('Это число')
-		break;
-	case 1: 
-		alert('Это число');
-		break;
-	case 2: 
-		alert('Это число');
-		break;	
-	case 3: 
-		alert('Это число');
-		break;		
-	case 4: 
-		alert('Это число');
-		break;
-	case 5: 
-		alert('Это число');
-		break;
-	case 6: 
-		alert('Это число');
-		break;
-	case 7: 
-		alert('Это число');
-		break;
-	case 8: 
-		alert('Это число');
-		break;
-	case 9: 
-		alert('Это число');
-		break;
-	case 10: 
-		alert('Это число');
-		break;
-	case 11: 
-		alert('Это число');
-		break;
-	case 12: 
-		alert('Это число');
-		break;
-	case 13: 
-		alert('Это число');
-		break;
-	case 14: 
-		alert('Это число');
-		break;
-	case 15: 
-		alert('Это число');
-		break;
-	default:
-	alert('Все, больше нет чисел!')
-}	
-
-//5
-function task(a,b) {
-	return a + b;
-}
-
-var r = task(9,12);
-	alert(r);
+// 2, 3
+var goods = [
+    {
+        title: "Товар 1",
+        price: 100,
+        count: 2
+    },
+    {
+        title: "Товар 2",
+        price: 200,
+        count: 2
+    },
+    {
+        title: "Товар 3",
+        price: 300,
+        count: 2
+    }
+];
 
 
-function sm(c, d) {
-	task(3, 2);
-	return c - d;
-}
+var s = 0;
+	for(var item of goods) {
+    	s = s + item.price;
+    		console.log(item.title + " стоит " + item.price);
+	}
 
-var e = sm(3, 2)
-	alert(e)
-
-
-function it(l,x) {
-	return l / x;
-}
-
-var s = it(14, 7)
-	alert(s)
+console.log("Общая сумма товаров = " + s);
 
 
-function ol(s,k) {
-	return s * k;
-}
-
-var t = ol(5, 5)
-	alert(t)
-
-//6
-function mathOperation(arg1, arg2, operation) {
-	switch(operation) {
-		case '*' : 
-
-		return ol(arg1, arg2);
+function countBasketPrice(countBasket) {
+	for(var item of goods) {
+		s = s + item.price
 	}
 }
+console.log('Сумма товаров: ' + s * item.count);
 
-var l = ol(9, 9);
-console.log(l);
+// 4
+for (var i = 0; i <= 9; console.log(i++));{}
 
-
-//7
- //Если null >= 0 будет истина, a другие > < ложью, cравнения преобразуют null в число, рассматривая его как 0.
-
- //8
- function power(val, pow) {
- 	if (val == 2) {
- 		return pow;
- 	} else {
- 		return val * power(pow, (val - 2));
-
- 	}
- }
+// 5
+var r = 'x';
+	for (var i = 0; i < 20; i++) {
+		r += 'x';
+			console.log(r);
+	}
