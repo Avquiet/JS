670d335904f3ea0f95dad327a4ab8bc5890f7931
Avquiet <78691610+Avquiet@users.noreply.github.com>
2021-06-15 14:21:43 +0300

    ADDED HW-2 ---- 15.6.21

--- main.js
+++ main.js
@@ -1,28 +1,100 @@
-const products = [
-    {id: 1, title: 'Notebook',  price: 2000},
-    {id: 2, title: 'Mouse', price: 20},
-    {id: 3, title: 'Keyboard', price: 200},
-    {id: 4, title: 'Gamepad', price: 50},
-];
+class ProductList{
+    constructor(container='.products'){
+        this.container = container;
+        this.goods = [];
+        this._fetchProducts();
+        this.render();//вывод товаров на страницу
+    }
+    _fetchProducts(){
+        this.goods = [
+            {id: 1, title: 'Notebook', price: 2000},
+            {id: 2, title: 'Mouse', price: 20},
+            {id: 3, title: 'Keyboard', price: 200},
+            {id: 4, title: 'Gamepad', price: 50},
+        ];
+    }
+    
+    render(){
+        const block = document.querySelector(this.container);
+        for(let product of this.goods){
+            const item = new ProductItem(product);
+             block.insertAdjacentHTML("beforeend",item.render());
+//           block.innerHTML += item.render();
+        }
+    }
 
-//Функция для формирования верстки каждого товара
-//Добавить в выводе изображение
+    goodsSum() {
+        
+        let sum = 0;
+        this.goods.forEach(item=> {
+            sum + item.price
+        })
 
-const renderProduct = (cards) => {
-    return `<div class="product-item">
-                <h3 class='item-1'>${cards.title}</h3>
-                <img src='9.jpg'>
-                <p class='item-2' >${cards.price}</p>
+    }
+    
+}
+class inBasket {
+
+    addInBas() {
+    }
+
+    removeBas(){
+
+    }
+
+    changeBas(){
+        
+    }
+
+}
+
+class infoBasket {
+    constructor() {
+   
+    }
+
+    render(){
+
+    }
+
+}
+
+
+class ProductItem{
+    constructor(product,img='https://via.placeholder.com/200x150'){
+        this.title = product.title;
+        this.id = product.id;
+        this.price = product.price;
+        this.img = img;
+    }
+    render(){
+           return `<div class="product-item">
+                <img src="${this.img}">
+                <h3>${this.title}</h3>
+                <p>${this.price}</p>
                 <button class="buy-btn">Купить</button>
             </div>`
-            
-};
+    }
+}
 
-const renderPage = list => {
-    const productsList = list.map(item => renderProduct(item)).join('');
-    console.log(productsList);
-    document.querySelector('.products').innerHTML = productsList;
-};
+let list = new ProductList();
 
 
-renderPage(products);
+//const products = [
+//    {id: 1, title: 'Notebook', price: 2000},
+//    {id: 2, title: 'Mouse', price: 20},
+//    {id: 3, title: 'Keyboard', price: 200},
+//    {id: 4, title: 'Gamepad', price: 50},
+//];
+//
+//const renderProduct = (product,img='https://placehold.it/200x150') => {
+//    return `<div class="product-item">
+//                <img src="${img}">
+//                <h3>${product.title}</h3>
+//                <p>${product.price}</p>
+//                <button class="buy-btn">Купить</button>
+//            </div>`
+//};
+//const renderPage = list => document.querySelector('.products').innerHTML = list.map(item => renderProduct(item)).join('');
+//
+//renderPage(products);
