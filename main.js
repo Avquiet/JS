a5dc83178960a8526efab1b6b434888e4d00cc16
Avquiet <78691610+Avquiet@users.noreply.github.com>
2021-06-19 23:58:37 +0300

    ADDED HW-3 ---- 19.6.21

--- main.js
+++ main.js
@@ -1,100 +1,141 @@
-class ProductList{
-    constructor(container='.products'){
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
+// let getRequest = (url, cb) => {
+//     let xhr = new XMLHttpRequest();
+//     // window.ActiveXObject -> xhr = new ActiveXObject()
+//     xhr.open("GET", url, true);
+//     xhr.onreadystatechange = () => {
+//         if(xhr.readyState === 4){
+//             if(xhr.status !== 200){
+//                 console.log('Error');
+//             } else {
+//                 cb(xhr.responseText);
+//             }
+//         }
+//     };
+//     xhr.send();
+// };
+
+class ProductsList {
+    constructor(container = '.products'){
         this.container = container;
-        this.goods = [];
-        this._fetchProducts();
-        this.render();//вывод товаров на страницу
+        this.goods = [];//массив товаров из JSON документа
+        this._getProducts()
+            .then(data => { //data - объект js
+                 this.goods = [...data];
+                 this.render()
+            });
     }
-    _fetchProducts(){
-        this.goods = [
-            {id: 1, title: 'Notebook', price: 2000},
-            {id: 2, title: 'Mouse', price: 20},
-            {id: 3, title: 'Keyboard', price: 200},
-            {id: 4, title: 'Gamepad', price: 50},
-        ];
+    // _fetchProducts(cb){
+    //     getRequest(`${API}/catalogData.json`, (data) => {
+    //         this.goods = JSON.parse(data);
+    //         console.log(this.goods);
+    //         cb();
+    //     })
+    // }
+    _getProducts(){
+      
+        return fetch(`${API}/catalogData.json`)
+            .then(result => result.json())
+            .catch(error => {
+                console.log(error);
+            })
     }
-    
+    calcSum(){
+        return this.allProducts.reduce((accum, item) => accum += item.price, 0);
+    }
     render(){
         const block = document.querySelector(this.container);
-        for(let product of this.goods){
-            const item = new ProductItem(product);
-             block.insertAdjacentHTML("beforeend",item.render());
-//           block.innerHTML += item.render();
+        for (let product of this.goods){
+            const productObj = new ProductItem(product);
+//            this.allProducts.push(productObj);
+            block.insertAdjacentHTML('beforeend', productObj.render());
         }
-    }
-
-    goodsSum() {
-        
-        let sum = 0;
-        this.goods.forEach(item=> {
-            sum + item.price
-        })
-
-    }
-    
-}
-class inBasket {
-
-    addInBas() {
-    }
-
-    removeBas(){
 
     }
 
-    changeBas(){
+    contents(){
         
     }
-
-}
-
-class infoBasket {
-    constructor() {
-   
-    }
-
-    render(){
-
-    }
-
 }
 
 
-class ProductItem{
-    constructor(product,img='https://via.placeholder.com/200x150'){
-        this.title = product.title;
-        this.id = product.id;
+class ProductItem {
+    constructor(product, img = 'https://via.placeholder.com/200x150'){
+        this.title = product.product_name;
         this.price = product.price;
+        this.id = product.id_product;
         this.img = img;
     }
     render(){
-           return `<div class="product-item">
-                <img src="${this.img}">
-                <h3>${this.title}</h3>
-                <p>${this.price}</p>
-                <button class="buy-btn">Купить</button>
+        return `<div class="product-item" data-id="${this.id}">
+                <img src="${this.img}" alt="Some img">
+                <div class="desc">
+                    <h3>${this.title}</h3>
+                    <p>${this.price} $</p>
+                    <button class="buy-btn">Купить</button>
+                </div>
             </div>`
     }
 }
+let list = new ProductsList();
 
-let list = new ProductList();
 
+class Parsel {
+    constructor(container = '.cards') {
+        this.container = container;
+        this.goods = [];
+        this._clickParce();
+        this._getParceItems()
 
-//const products = [
-//    {id: 1, title: 'Notebook', price: 2000},
-//    {id: 2, title: 'Mouse', price: 20},
-//    {id: 3, title: 'Keyboard', price: 200},
-//    {id: 4, title: 'Gamepad', price: 50},
-//];
-//
-//const renderProduct = (product,img='https://placehold.it/200x150') => {
-//    return `<div class="product-item">
-//                <img src="${img}">
-//                <h3>${product.title}</h3>
-//                <p>${product.price}</p>
-//                <button class="buy-btn">Купить</button>
-//            </div>`
-//};
-//const renderPage = list => document.querySelector('.products').innerHTML = list.map(item => renderProduct(item)).join('');
-//
-//renderPage(products);
+        .then (data => {
+            this.goods = [...data.contents];
+            this.render ()
+        })
+    }
+
+    _getParceItems() {
+        
+        return fetch(`${API}/getBasket.json`)
+        .then(result => result.json())
+        .catch(error => {
+            console.log(error);
+        })
+
+    }
+
+    render(){
+        const box = document.querySelector(this.container);
+        for(let product of this.goods) {
+            const pBoxes = new ParseItem();
+            box.insertAdjacentHTML('beforeend', pBoxes.render(product));
+        }
+
+    }
+
+    _clickParce() {
+        document.querySelector('.btn-cart').addEventListener('click', () => {
+            document.querySelector(this.container).classList.toggle('invisible');
+        });    
+    }   
+}
+    class ParseItem {
+        render(product) {
+            return `<div class="cart-item" data-id="${product.id_product}">
+                <div class="product-bio">
+                <img src="${product.img}" alt="Some image">
+                <div class="product-desc">
+                <p class="product-title">${product.product_name}</p>
+                <p class="product-quantity">Quantity: ${product.quantity}</p>
+            <p class="product-single-price">$${product.price} each</p>
+            </div>
+            </div>
+            <div class="right-block">
+                <p class="product-price">$${product.quantity * product.price}</p>
+                <button class="del-btn" data-id="${product.id_product}">&times;</button>
+            </div>
+            </div>`
+        }
+    }
+
+let cor = new Parsel();
